c20612f0ab7219fb1b81c628284079cbdaa223af
Israel906 <israelword4@gmail.com>
2024-11-07 16:51:39 +0300

    Add files via upload

---
+++ CAT_Q3.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+int i,j,k,l;
+
+int marks[2][2]={{{65,92},{84,72}},{{35,70},{59,67}}};
+
+for(i<0;i<2;i++){
+for(j<0;j<2;j++){
+for(k<0;k<2;k++){
+for(l<0;l<2;l++){
+printf("marks[%d][%d]=%d\marks",i,j,marks[i][j]);
+printf("marks[%d][%d]=%d\marks",k,l,marks[k][l]);
+}
+}
+}
+}
+
+
+
+
+
+
+}
